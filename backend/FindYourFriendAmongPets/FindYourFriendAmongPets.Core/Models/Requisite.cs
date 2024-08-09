fa8213738f3afdce0c60c8ad512ea494928746ab
Slava <v.vorobjev@hq.runpay.com>
2024-08-09 17:02:52 +0300

    ID-B-2-created-model-pet

---
+++ backend/FindYourFriendAmongPets/FindYourFriendAmongPets.Core/Models/Requisite.cs
@@ -0,0 +1,10 @@
+﻿namespace FindYourFriendAmongPets.Core.Models;
+
+public class Requisite
+{  
+    public Guid Id { get; }
+    
+    public string Name { get; }
+    
+    public string Description { get; }
+}
