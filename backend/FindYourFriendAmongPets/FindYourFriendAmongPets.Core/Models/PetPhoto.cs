2fa6a3a8f0420be78a0daa0c64ae7b2100a31731
Slava <v.vorobjev@hq.runpay.com>
2024-08-13 17:50:53 +0300

    ID-B-4-add-PetPhoto

---
+++ backend/FindYourFriendAmongPets/FindYourFriendAmongPets.Core/Models/PetPhoto.cs
@@ -0,0 +1,27 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace FindYourFriendAmongPets.Core.Models;
+
+public record PetPhoto
+{
+    private PetPhoto(PetPhotoId id, string pathToStorage, bool isMain)
+    {
+        Id = id;
+        PathToStorage = pathToStorage;
+        IsMain = isMain;
+    }
+
+    public PetPhotoId Id { get; }
+
+    public string PathToStorage { get; }
+
+    public bool IsMain { get; }
+
+    public static Result<PetPhoto> Create(PetPhotoId id, string pathToStorage, bool isMain)
+    {
+        if (string.IsNullOrWhiteSpace(pathToStorage))
+            return Result.Failure<PetPhoto>($"{nameof(PathToStorage)} can not be empty.");
+
+        return Result.Success(new PetPhoto(id, pathToStorage, isMain));
+    }
+}
---
+++ backend/FindYourFriendAmongPets/FindYourFriendAmongPets.Core/Models/PetPhotoId.cs
@@ -0,0 +1,14 @@
+﻿namespace FindYourFriendAmongPets.Core.Models;
+
+public record PetPhotoId
+{
+    public PetPhotoId(Guid value)
+    {
+        Value = value;
+    }
+    public Guid Value { get; }
+    
+    public static PetPhotoId NewPetPhotoId() => new(Guid.NewGuid());
+
+    public static PetPhotoId Empty() => new(Guid.Empty);
+}
