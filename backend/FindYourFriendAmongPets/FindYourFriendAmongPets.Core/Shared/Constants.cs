8ae90e966f89b36d6272c9c623464cedc8497244
Slava <v.vorobjev@hq.runpay.com>
2024-08-14 15:55:11 +0300

    ID-B-4
    refactoring

---
+++ backend/FindYourFriendAmongPets/FindYourFriendAmongPets.Core/Shared/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace FindYourFriendAmongPets.Core.Shared;
+
+public class Constants
+{
+    public const int MAX_TITLE_LENGHT = 100;
+    
+    public const int MAX_DESCRIPTION_LENGHT = 2000;
+    
+    public const int MAX_PHONENUMBER_LENGHT = 13;
+    
+}
