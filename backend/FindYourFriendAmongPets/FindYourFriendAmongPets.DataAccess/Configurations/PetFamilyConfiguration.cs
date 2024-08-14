243127760af95d3d52ebb12121cbec15b4b3b424
Slava <v.vorobjev@hq.runpay.com>
2024-08-14 09:51:08 +0300

    ID-B-4.1
    add PetFamilyConfiguration and PetFamilyDbContext

---
+++ backend/FindYourFriendAmongPets/FindYourFriendAmongPets.DataAccess/Configurations/PetFamilyConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using FindYourFriendAmongPets.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FindYourFriendAmongPets.DataAccess.Configurations;
+
+public class PetFamilyConfiguration:IEntityTypeConfiguration<Volunteer>
+{
+    public void Configure(EntityTypeBuilder<Volunteer> builder)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ backend/FindYourFriendAmongPets/FindYourFriendAmongPets.DataAccess/PetFamilyDbContext.cs
@@ -0,0 +1,21 @@
+﻿using FindYourFriendAmongPets.Core.Models;
+using FindYourFriendAmongPets.DataAccess.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace FindYourFriendAmongPets.DataAccess;
+
+public class PetFamilyDbContext : DbContext
+{
+    public PetFamilyDbContext(DbContextOptions<PetFamilyDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Volunteer> Volunteers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new PetFamilyConfiguration());
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
