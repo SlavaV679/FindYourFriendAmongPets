b3e95c2e23aba609698bb2d88029c4381f5436d8
Slava <v.vorobjev@hq.runpay.com>
2024-08-15 08:50:05 +0300

    ID-B-4.1
    configurate DbContext

--- backend/FindYourFriendAmongPets/FindYourFriendAmongPets.DataAccess/PetFamilyDbContext.cs
+++ backend/FindYourFriendAmongPets/FindYourFriendAmongPets.DataAccess/PetFamilyDbContext.cs
@@ -1,22 +1,38 @@
 ﻿using FindYourFriendAmongPets.Core.Models;
 using FindYourFriendAmongPets.DataAccess.Configurations;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace FindYourFriendAmongPets.DataAccess;
 
 public class PetFamilyDbContext : DbContext
 {
-    public PetFamilyDbContext(DbContextOptions<PetFamilyDbContext> options) : base(options)
+    private const string PET_FAMILY_DATABASE = "PetFamilyDb";
+    public readonly IConfiguration _Configuration;
+    public PetFamilyDbContext(DbContextOptions<PetFamilyDbContext> options, IConfiguration configuration) : base(options)
     {
+        _Configuration = configuration;
     }
 
     public DbSet<Volunteer> Volunteers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(_Configuration.GetConnectionString(PET_FAMILY_DATABASE));
+        optionsBuilder.UseSnakeCaseNamingConvention();
+        optionsBuilder.UseLoggerFactory(CreateLoggerFactory());
+        //base.OnConfiguring(optionsBuilder);
+    }
+
+    private ILoggerFactory CreateLoggerFactory()=>
+        LoggerFactory.Create(builder => { builder.AddConsole(); });
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfiguration(new VolunteerConfiguration());
         modelBuilder.ApplyConfiguration(new PetConfiguration());
 
-        base.OnModelCreating(modelBuilder);
+        //base.OnModelCreating(modelBuilder);
     }
 }
