c392b1bec51d13b52119b9cb64ebd2f106fef361
Slava <v.vorobjev@hq.runpay.com>
2024-08-16 16:21:22 +0300

    ID-B-4.1
    refactoring Configurations

--- backend/FindYourFriendAmongPets/FindYourFriendAmongPets.DataAccess/Configurations/PetConfiguration.cs
+++ backend/FindYourFriendAmongPets/FindYourFriendAmongPets.DataAccess/Configurations/PetConfiguration.cs
@@ -17,8 +17,49 @@ public class PetConfiguration : IEntityTypeConfiguration<Pet>
             .HasConversion(id => id.Value,
                 value => PetId.Create(value));
 
+        builder.Property(p => p.Species)
+            .HasMaxLength(Constants.MAX_LOW_TEXT_LENGHT)
+            .IsRequired();
+        
         builder.Property(p => p.Description)
             .HasMaxLength(Constants.MAX_DESCRIPTION_LENGHT)
+            .IsRequired();
+        
+        builder.Property(p => p.Breed)
+                    .HasMaxLength(Constants.MAX_LOW_TEXT_LENGHT)
+                    .IsRequired();
+        
+        builder.Property(p => p.Color)
+                    .HasMaxLength(Constants.MAX_LOW_TEXT_LENGHT)
+                    .IsRequired();
+        
+        builder.Property(p => p.HealthInfo)
+                    .HasMaxLength(Constants.MAX_LOW_TEXT_LENGHT)
+                    .IsRequired();
+        
+        builder.ComplexProperty(v => v.Address, address =>
+        {
+            address.Property(f => f.City)
+                .HasMaxLength(Constants.MAX_LOW_TEXT_LENGHT)
+                .HasColumnName("city")
+                .IsRequired();
+            address.Property(f => f.Street)
+                .HasMaxLength(Constants.MAX_LOW_TEXT_LENGHT)
+                .HasColumnName("street")
+                .IsRequired();
+            address.Property(f => f.Building)
+                .HasMaxLength(Constants.MAX_LOW_TEXT_LENGHT)
+                .HasColumnName("building");
+            address.Property(f => f.Description)
+                .HasMaxLength(Constants.MAX_LOW_TEXT_LENGHT)
+                .HasColumnName("address_description");
+            address.Property(f => f.Country)
+                .HasMaxLength(Constants.MAX_LOW_TEXT_LENGHT)
+                .HasColumnName("country");
+        });
+        
+        builder.Property(p => p.OwnersPhoneNumber)
+            .HasMaxLength(Constants.MAX_PHONENUMBER_LENGHT)
             .IsRequired();
         
         builder.Property(p=>p.HelpStatus)
@@ -26,18 +67,25 @@ public class PetConfiguration : IEntityTypeConfiguration<Pet>
                 v => v.ToString(),
                 v => (Status)Enum.Parse(typeof(Status), v));
 
-        builder.HasMany(p => p.Requisites)
-            .WithOne()
-            .HasForeignKey("requisite_id");
-
         builder.OwnsOne(p => p.Details, petBuilder =>
         {
             petBuilder.ToJson();
+            
             petBuilder.OwnsMany(d => d.PetPhotos, photoBuilder =>
             {
                 photoBuilder.Property(pp => pp.PathToStorage)
                     .IsRequired()
                     .HasMaxLength(Constants.MAX_PATH_TO_STORAGE_LENGHT);
+            });
+            
+            petBuilder.OwnsMany(d => d.Requisites, requisitesBuilder =>
+            {
+                requisitesBuilder.Property(r => r.Name)
+                    .IsRequired()
+                    .HasMaxLength(Constants.MAX_LOW_TEXT_LENGHT);
+                requisitesBuilder.Property(r => r.Description)
+                    .IsRequired()
+                    .HasMaxLength(Constants.MAX_HIGH_TEXT_LENGHT);
             });
         });
     }
--- backend/FindYourFriendAmongPets/FindYourFriendAmongPets.DataAccess/PetFamilyDbContext.cs
+++ backend/FindYourFriendAmongPets/FindYourFriendAmongPets.DataAccess/PetFamilyDbContext.cs
@@ -6,26 +6,30 @@ using Microsoft.Extensions.Logging;
 
 namespace FindYourFriendAmongPets.DataAccess;
 
-public class PetFamilyDbContext : DbContext
+public class PetFamilyDbContext(IConfiguration _configuration) : DbContext
 {
     private const string PET_FAMILY_DATABASE = "PetFamilyDb";
-    public readonly IConfiguration _Configuration;
-    public PetFamilyDbContext(DbContextOptions<PetFamilyDbContext> options, IConfiguration configuration) : base(options)
-    {
-        _Configuration = configuration;
-    }
 
-    public DbSet<Volunteer> Volunteers { get; set; }
+    //TODO разобраться почему с этим конструктором не работает
+    // public readonly IConfiguration _configuration;
+    //
+    // public PetFamilyDbContext(DbContextOptions<PetFamilyDbContext> options, IConfiguration configuration) : base(options)
+    // {
+    //     _configuration = configuration;
+    // }
+
+    //public DbSet<Volunteer> Volunteers { get; set; } // чем отличается эти два вида объявления?
+    public DbSet<Volunteer> Volunteers => Set<Volunteer>();
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseNpgsql(_Configuration.GetConnectionString(PET_FAMILY_DATABASE));
+        optionsBuilder.UseNpgsql(_configuration.GetConnectionString(PET_FAMILY_DATABASE));
         optionsBuilder.UseSnakeCaseNamingConvention();
         optionsBuilder.UseLoggerFactory(CreateLoggerFactory());
-        //base.OnConfiguring(optionsBuilder);
+        base.OnConfiguring(optionsBuilder);
     }
 
-    private ILoggerFactory CreateLoggerFactory()=>
+    private ILoggerFactory CreateLoggerFactory() =>
         LoggerFactory.Create(builder => { builder.AddConsole(); });
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -33,6 +37,6 @@ public class PetFamilyDbContext : DbContext
         modelBuilder.ApplyConfiguration(new VolunteerConfiguration());
         modelBuilder.ApplyConfiguration(new PetConfiguration());
 
-        //base.OnModelCreating(modelBuilder);
+        base.OnModelCreating(modelBuilder);
     }
 }
