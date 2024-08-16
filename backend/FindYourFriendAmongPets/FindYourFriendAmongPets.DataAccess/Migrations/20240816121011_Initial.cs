bb01d4307bad82642f88d3101b3a9162d06cb8b5
Slava <v.vorobjev@hq.runpay.com>
2024-08-16 16:24:46 +0300

    ID-B-4.1-create-migrations

---
+++ backend/FindYourFriendAmongPets/FindYourFriendAmongPets.DataAccess/Migrations/20240816121011_Initial.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FindYourFriendAmongPets.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "volunteers",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    description = table.Column<string>(type: "character varying(2000)", maxLength: 2000, nullable: false),
+                    experience_in_years = table.Column<int>(type: "integer", nullable: false),
+                    count_pets_realized = table.Column<int>(type: "integer", nullable: false),
+                    count_pets_looking_for_home = table.Column<int>(type: "integer", nullable: false),
+                    count_pets_healing = table.Column<int>(type: "integer", nullable: false),
+                    phone_number = table.Column<string>(type: "character varying(13)", maxLength: 13, nullable: false),
+                    first_name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    last_name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    patronymic = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_volunteers", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "pets",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    species = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    description = table.Column<string>(type: "character varying(2000)", maxLength: 2000, nullable: false),
+                    breed = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    color = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    health_info = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    weight = table.Column<double>(type: "double precision", nullable: false),
+                    height = table.Column<double>(type: "double precision", nullable: false),
+                    owners_phone_number = table.Column<string>(type: "character varying(13)", maxLength: 13, nullable: false),
+                    is_neutered = table.Column<bool>(type: "boolean", nullable: false),
+                    date_of_birth = table.Column<DateOnly>(type: "date", nullable: false),
+                    is_vaccinated = table.Column<bool>(type: "boolean", nullable: false),
+                    help_status = table.Column<string>(type: "text", nullable: false),
+                    date_created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    volunteer_id = table.Column<Guid>(type: "uuid", nullable: true),
+                    building = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    city = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    country = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    address_description = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    street = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    details = table.Column<string>(type: "jsonb", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_pets", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_pets_volunteers_volunteer_id",
+                        column: x => x.volunteer_id,
+                        principalTable: "volunteers",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "requisite_for_help",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false),
+                    volunteer_id = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_requisite_for_help", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_requisite_for_help_volunteers_volunteer_id",
+                        column: x => x.volunteer_id,
+                        principalTable: "volunteers",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "social_network",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    volunteer_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    title = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    link = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_social_network", x => new { x.volunteer_id, x.id });
+                    table.ForeignKey(
+                        name: "fk_social_network_volunteers_volunteer_id",
+                        column: x => x.volunteer_id,
+                        principalTable: "volunteers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_pets_volunteer_id",
+                table: "pets",
+                column: "volunteer_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_requisite_for_help_volunteer_id",
+                table: "requisite_for_help",
+                column: "volunteer_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "pets");
+
+            migrationBuilder.DropTable(
+                name: "requisite_for_help");
+
+            migrationBuilder.DropTable(
+                name: "social_network");
+
+            migrationBuilder.DropTable(
+                name: "volunteers");
+        }
+    }
+}
