58811790753fbae86716d840f28e3d85bf823e4e
Slava <v.vorobjev@hq.runpay.com>
2024-08-26 09:35:24 +0300

    ID-B-5.2-Envelope-get-IEnumerable<Error>

--- backend/FindYourFriendAmongPets/FindYourFriendAmongPets.API/Extensions/ResponseExtensions.cs
+++ backend/FindYourFriendAmongPets/FindYourFriendAmongPets.API/Extensions/ResponseExtensions.cs
@@ -7,28 +7,6 @@ namespace FindYourFriendAmongPets.API.Extensions;
 
 public static class ResponseExtensions
 {
-    public static ActionResult ToResponse(this UnitResult<Error> result)
-    {
-        if (result.IsSuccess)
-            return new OkResult();
-
-        var statusCode = result.Error.Type switch
-        {
-            ErrorType.Validation => StatusCodes.Status400BadRequest,
-            ErrorType.NotFound => StatusCodes.Status404NotFound,
-            ErrorType.Conflict => StatusCodes.Status409Conflict,
-            ErrorType.Failure => StatusCodes.Status500InternalServerError,
-            _ => StatusCodes.Status500InternalServerError
-        };
-
-        var envelope = Envelope.Error(result.Error);
-
-        return new ObjectResult(envelope)
-        {
-            StatusCode = statusCode
-        };
-    }
-
     public static ActionResult<T> ToResponse<T>(this Result<T, Error> result)
     {
         if (result.IsSuccess)
@@ -43,7 +21,9 @@ public static class ResponseExtensions
             _ => StatusCodes.Status500InternalServerError
         };
 
-        var envelope = Envelope.Error(result.Error);
+        var responseError = new ResponseError(result.Error.Code, result.Error.Message, null);
+        
+        var envelope = Envelope.Error([responseError]);
 
         return new ObjectResult(envelope)
         {
