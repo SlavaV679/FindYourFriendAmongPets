bbc9b4718d48b2b5860d8fbd949c90c9c4281641
Slava <v.vorobjev@hq.runpay.com>
2024-08-26 10:12:42 +0300

    ID-B-5.2-Add-FluentValidation

---
+++ backend/FindYourFriendAmongPets/FindYourFriendAmongPets.Application/Volunteers/Create/CreateVolunteerRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using FindYourFriendAmongPets.Application.Validation;
+using FindYourFriendAmongPets.Core.Models;
+using FluentValidation;
+
+namespace FindYourFriendAmongPets.Application.Volunteers.Create;
+
+public class CreateVolunteerRequestValidator : AbstractValidator<CreateVolunteerRequest>
+{
+    public CreateVolunteerRequestValidator()
+    {
+        RuleFor(r => new { r.FirstName, r.LastName, r.Patronymic })
+            .MustBeValueObject(f => FullName.Create(f.FirstName,f.LastName,f.Patronymic));
+        
+        RuleFor(c => c.Description)
+            .MustBeValueObject(Description.Create);
+        
+        RuleFor(c => c.PhoneNumber)
+            .MustBeValueObject(PhoneNumber.Create);
+    }
+}
