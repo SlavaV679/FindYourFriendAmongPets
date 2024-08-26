2f84052d7badfd0a032534b029199575884ab62b
Slava <v.vorobjev@hq.runpay.com>
2024-08-26 10:30:50 +0300

    ID-B-5.2-Add-Extension-Error

--- backend/FindYourFriendAmongPets/FindYourFriendAmongPets.API/Extensions/ResponseExtensions.cs
+++ backend/FindYourFriendAmongPets/FindYourFriendAmongPets.API/Extensions/ResponseExtensions.cs
@@ -30,4 +30,25 @@ public static class ResponseExtensions
             StatusCode = statusCode
         };
     }
+
+    public static ActionResult ToResponse(this Error error)
+    {
+        var statusCode = error.Type switch
+        {
+            ErrorType.Validation => StatusCodes.Status400BadRequest,
+            ErrorType.NotFound => StatusCodes.Status404NotFound,
+            ErrorType.Conflict => StatusCodes.Status409Conflict,
+            ErrorType.Failure => StatusCodes.Status500InternalServerError,
+            _ => StatusCodes.Status500InternalServerError
+        };
+        
+        var responseError = new ResponseError(error.Code, error.Message, null);
+        
+        var envelope = Envelope.Error([responseError]);
+
+        return new ObjectResult(envelope)
+        {
+            StatusCode = statusCode
+        };
+    }
 }
