954b425752103fed2a2222de6e707cb7c2a4d419
Slava <v.vorobjev@hq.runpay.com>
2024-08-28 13:16:13 +0300

    ID-B-5.2-Add-FluentValidation

--- backend/FindYourFriendAmongPets/FindYourFriendAmongPets.API/Controllers/VolunteerController.cs
+++ backend/FindYourFriendAmongPets/FindYourFriendAmongPets.API/Controllers/VolunteerController.cs
@@ -1,8 +1,10 @@
 ﻿using CSharpFunctionalExtensions;
 using FindYourFriendAmongPets.API.Extensions;
+using FindYourFriendAmongPets.API.Response;
 using FindYourFriendAmongPets.Application.Volunteers.Create;
 using FindYourFriendAmongPets.Core.Models;
 using FindYourFriendAmongPets.Core.Shared;
+using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FindYourFriendAmongPets.API.Controllers;
@@ -13,10 +15,25 @@ public class VolunteerController : ControllerBase
 {
     [HttpPost]
     public async Task<ActionResult<Guid>> Create(
-        [FromServices] CreateVolunteerHandler handler,
+        [FromServices] CreateVolunteerHandler handler, [FromServices] IValidator<CreateVolunteerRequest> validator,
         [FromBody] CreateVolunteerRequest request,
         CancellationToken cancellationToken)
     {
+        var validationResult = await validator.ValidateAsync(request, cancellationToken);
+
+        if (validationResult.IsValid == false)
+        {
+            var validationErrors = validationResult.Errors;
+
+            var errors = from validationError in validationErrors
+                let error = Error.Validation(validationError.ErrorCode, validationError.ErrorMessage)
+                select new ResponseError(error.Code, error.Message, validationError.PropertyName);
+
+            var envelope = Envelope.Error(errors);
+
+            return BadRequest(envelope);
+        }
+
         var response = await handler.Handle(request, cancellationToken);
 
         return response.ToResponse();
