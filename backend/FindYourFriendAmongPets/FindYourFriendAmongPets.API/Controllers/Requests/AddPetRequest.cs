0cd7a7a126ceb7f6ccb84dcc74ea550f4903825a
vvorobjev <v.vorobjev@hq.runpay.com>
2024-09-13 16:18:22 +0300

    ID-B-7.1-add-Requests-to-API

---
+++ backend/FindYourFriendAmongPets/FindYourFriendAmongPets.API/Controllers/Requests/AddPetRequest.cs
@@ -0,0 +1,40 @@
+﻿using FindYourFriendAmongPets.Application.Volunteers.AddPet;
+using FindYourFriendAmongPets.Core.Models;
+
+namespace FindYourFriendAmongPets.API.Controllers.Requests;
+
+public record AddPetRequest(
+    string Name,
+    PetSpeciesDto PetSpecies,
+    string Description,
+    string Color,
+    string HealthInfo,
+    AddressDto Address,
+    double Weight,
+    double Height,
+    string OwnersPhoneNumber,
+    bool IsNeutered,
+    DateTime DateOfBirth,
+    bool IsVaccinated,
+    Status HelpStatus,
+    //PetRequisiteDetails RequisiteDetails,
+    IFormFileCollection Files)
+{
+    public AddPetCommand ToCommand(Guid volunteerId, IEnumerable<CreateFileCommand> fileContents) =>
+        new(
+            volunteerId,
+            Name,
+            PetSpecies,
+            Description,
+            Color,
+            HealthInfo,
+            Address,
+            Weight,
+            Height,
+            OwnersPhoneNumber,
+            IsNeutered,
+            DateOfBirth,
+            IsVaccinated,
+            HelpStatus,
+            fileContents);
+}
---
+++ backend/FindYourFriendAmongPets/FindYourFriendAmongPets.API/Controllers/Requests/UpdateMainInfoRequest.cs
@@ -0,0 +1,19 @@
+﻿using FindYourFriendAmongPets.Application.Volunteers.Shared;
+using FindYourFriendAmongPets.Application.Volunteers.UpdateMainInfo;
+
+namespace FindYourFriendAmongPets.API.Controllers.Requests;
+
+public record UpdateMainInfoRequest(
+    FullNameDto FullName,
+    string Description,
+    string PhoneNumber,
+    int ExperienceInYears)
+{
+    public UpdateMainInfoCommand ToCommand(Guid volunteerId) =>
+        new(
+            volunteerId,
+            FullName,
+            Description,
+            PhoneNumber,
+            ExperienceInYears);
+}
