e790c72b990375f21de0da72b690a98b4d49a5c5
vvorobjev <v.vorobjev@hq.runpay.com>
2024-10-04 09:22:06 +0300

    ID-B-9.3-add-PagetList<T>

---
+++ backend/FindYourFriendAmongPets/FindYourFriendAmongPets.Application/Extensions/QueriesExtensions.cs
@@ -0,0 +1,29 @@
+﻿using FindYourFriendAmongPets.Application.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FindYourFriendAmongPets.Application.Extensions;
+
+public static class QueriesExtensions
+{
+    public static async Task<PagedList<T>> ToPagedList<T>(
+        this IQueryable<T> source,
+        int page,
+        int pageSize,
+        CancellationToken cancellationToken = default)
+    {
+        var totalCount = await source.CountAsync(cancellationToken);
+        
+        var items = await source
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync(cancellationToken: cancellationToken);
+        
+        return new PagedList<T>
+        {
+            Items = items,
+            PageSize = pageSize,
+            Page = page,
+            TotalCount = totalCount
+        };
+    }
+}
