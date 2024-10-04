0e15d2c78fdf75bd1b6faf097fe9f3b36595054c
vvorobjev <v.vorobjev@hq.runpay.com>
2024-10-04 09:04:24 +0300

    ID-B-9.3-add-general-ApplicationController

---
+++ backend/FindYourFriendAmongPets/FindYourFriendAmongPets.API/Controllers/ApplicationController.cs
@@ -0,0 +1,16 @@
+﻿using FindYourFriendAmongPets.API.Response;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FindYourFriendAmongPets.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public abstract class ApplicationController : ControllerBase
+{
+    public override OkObjectResult Ok(object? value)
+    {
+        var envelope = Envelope.Ok(value);
+        
+        return base.Ok(envelope);
+    }
+}
