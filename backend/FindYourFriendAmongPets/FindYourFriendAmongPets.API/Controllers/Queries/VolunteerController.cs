b6fdb300fc0a5af3bf452909c6913bd5831143d3
vvorobjev <v.vorobjev@hq.runpay.com>
2024-10-15 09:08:50 +0300

    ID-B-10(11)-GetVolunteerById using Dapper

--- backend/FindYourFriendAmongPets/FindYourFriendAmongPets.API/Controllers/Queries/VolunteerController.cs
+++ backend/FindYourFriendAmongPets/FindYourFriendAmongPets.API/Controllers/Queries/VolunteerController.cs
@@ -1,4 +1,5 @@
 ﻿using FindYourFriendAmongPets.API.Controllers.Queries.Requests;
+using FindYourFriendAmongPets.Application.Volunteers.Queries.GetVolunteerById;
 using FindYourFriendAmongPets.Application.Volunteers.Queries.GetVolunteersWithPagination;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,6 +14,20 @@ public class VolunteerController: ApplicationController
         CancellationToken cancellationToken)
     {
         var query = request.ToQuery();
+        
+        var response = await handler.Handle(query, cancellationToken);
+        
+        return Ok(response);
+    }
+    
+    [HttpGet("{id:guid}/dapper")]
+    public async Task<ActionResult> GetByIdDapper(
+        [FromRoute] Guid id,
+        // [FromQuery] GetVolunteerByIdRequest request,
+        [FromServices] GetVolunteerByIdHandler handler,
+        CancellationToken cancellationToken)
+    {
+        var query = new GetVolunteerByIdQuery(id);
         
         var response = await handler.Handle(query, cancellationToken);
         
