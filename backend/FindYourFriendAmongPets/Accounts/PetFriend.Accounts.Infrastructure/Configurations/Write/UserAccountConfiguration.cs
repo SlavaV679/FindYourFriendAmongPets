7aaaebff782a02193144b132e84a4e1e4eec81f2
vvorobjev <v.vorobjev@hq.runpay.com>
2024-11-11 08:43:39 +0300

    ID-B-18-add-DbContext-and-Configuration

---
+++ backend/FindYourFriendAmongPets/Accounts/PetFriend.Accounts.Infrastructure/Configurations/Write/UserAccountConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetFriend.Accounts.Domain;
+using PetFriend.SharedKernel;
+
+namespace PetFriend.Accounts.Infrastructure.Configurations.Write;
+
+public class UserAccountConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasOne(u => u.Role)
+            .WithMany(r => r.Users)
+            .HasForeignKey(u => u.RoleId)
+            .OnDelete(DeleteBehavior.Cascade);
+        
+        builder.ComplexProperty(u => u.FullName, fullName =>
+        {
+            fullName.Property(f => f.FirstName)
+                .HasMaxLength(Constants.MAX_LOW_TEXT_LENGHT)
+                .HasColumnName("first_name");
+            fullName.Property(f => f.LastName)
+                .HasMaxLength(Constants.MAX_LOW_TEXT_LENGHT)
+                .HasColumnName("last_name");
+            fullName.Property(f => f.Patronymic)
+                .HasMaxLength(Constants.MAX_LOW_TEXT_LENGHT)
+                .HasColumnName("patronymic");
+        });
+    }
+}
---
+++ backend/FindYourFriendAmongPets/Accounts/PetFriend.Accounts.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PetFriend.Accounts.Infrastructure.DbContexts;
+
+namespace PetFriend.Accounts.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddAccountsInfrastructure(
+        this IServiceCollection collection, IConfiguration configuration)
+    {
+        return collection;
+    }
+    
+    private static IServiceCollection AddDbContexts(this IServiceCollection services)
+    {
+        services.AddScoped<AccountsWriteDbContext>();
+
+        return services;
+    }
+    
+    private static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        //services.AddScoped<IVolunteerRepository, VolunteerRepository>();
+        
+        return services;
+    }
+}
